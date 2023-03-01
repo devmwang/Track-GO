c1d4dd6f7417bd65350d50e70e0df6fbe3ce7506
Michael Wang <mwang60@students.cs.ubc.ca>
2023-03-01 13:38:47 -0800

    Initial setup of StoreWriter

---
+++ src/main/persistence/StoreWriter.java
@@ -0,0 +1,40 @@
+// Code adapted from CPSC 210 JSON Serialization Demo at
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo/blob/master/src/main/persistence/JsonWriter.java
+
+package persistence;
+
+import java.io.PrintWriter;
+import org.json.JSONObject;
+
+import model.AppData;
+
+// Represents a writer that writes app data to a JSON file
+public class StoreWriter {
+    private static final int INDENT = 4;
+    private PrintWriter writer;
+    private String filePath;
+
+    // EFFECTS: Constructs writer to write data to file at filePath
+    public StoreWriter(String filePath) {
+        this.filePath = filePath;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Writes JSON representation of appData to file
+    public void write(AppData appData) {
+        JSONObject json = appData.toJson();
+        saveToFile(json.toString(INDENT));
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Closes writer
+    public void close() {
+        writer.close();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Writes data string to file
+    private void saveToFile(String data) {
+        writer.print(data);
+    }
+}
