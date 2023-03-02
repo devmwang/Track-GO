8aef0972cd271bbd22f7283efe94f6da0b8195b8
Michael Wang <mwang60@students.cs.ubc.ca>
2023-03-01 22:57:43 -0800

    Update main

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,6 +1,8 @@
 package ui;
 
+// Represents the main initialization point of the program
 public class Main {
+    // EFFECTS: Runs a program that tracks a variety of CS:GO-related stats with a console-based user interface
     public static void main(String[] args) {
         new ConsoleInterface();
     }
